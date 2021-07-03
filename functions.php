8c12114bc57db5ce48b71d93b0792af6dcb00dcf
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2021-07-03 17:38:37 -0400

    sparkles: Completed: WP-pages

--- functions.php
+++ functions.php
@@ -1,12 +1,21 @@
 <?php
-	function uni_files() {
-		wp_enqueue_script('scripts', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0.0', true);
+function uni_files() {
+	wp_enqueue_script('scripts', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0.0', true);
 
-		wp_enqueue_style('google-fonts', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+	wp_enqueue_style('google-fonts', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
 
-		wp_enqueue_style('font-awesome', "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+	wp_enqueue_style('font-awesome', "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
 
-		wp_enqueue_style('uni_styles', get_stylesheet_uri());
-	}
-	add_action('wp_enqueue_scripts', 'uni_files');
-?>
+	wp_enqueue_style('uni_styles', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'uni_files');
+
+function uni_features() {
+	add_theme_support('title-tag');
+	register_nav_menu('header_menu', "Header Menu");
+	register_nav_menu('explore_menu', "Explore Menu");
+	register_nav_menu('learn_menu', "Learn Menu");
+}
+
+add_action('after_setup_theme', 'uni_features');
+
